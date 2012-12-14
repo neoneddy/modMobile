90b2029c3c22cdee2a8ec88ce2ad3a0345448018
Josh Gulledge <jgulledge19@hotmail.com>
2012-12-14 17:11:58 -0500

    Fixed #1

--- core/components/modmobile/elements/plugins/modmobile.plugin.php
+++ core/components/modmobile/elements/plugins/modmobile.plugin.php
@@ -21,32 +21,34 @@ $get_var = $modx->getOption('modmobile.get_var');
 // just assigns a PlaceHolder to modmobile.get_var which can be used via an [[If]] in a template/chunck/snippet
 $use_if = $modx->getOption('modmobile.use_if');
 
-require_once MODX_CORE_PATH.'components/modMobile/model/detectmobile.class.php';
-
-//only proceed when there is a mobile theme
-if( !empty($use_if) && $use_if ) {
-    $mobile = new DetectMobile();
-    if ( $mobile->run() == 'mobile' ){
-        $modx->setPlaceholder($get_var, 'mobile');
-        $_SESSION[$get_var] = 'mobile';
-    } else {
-        $modx->setPlaceholder($get_var, 'full');
-        $_SESSION[$get_var] = 'full';
-    }
-    return;
-}
+require_once MODX_CORE_PATH.'components/modmobile/model/detectmobile.class.php';
 
 // get mobile theme setting
-$mobile_template = $modx->getOption('modmobile.mobile_template');
-//only proceed when there is a mobile theme
-if( !empty($mobile_template) ) {
-    $mobile = new DetectMobile();
-    
-    if ( $mobile->run() == 'mobile' ){
-        $modx->resource->set('template', $mobile_template);
+$mobile_template = $modx->getOption('modmobile.mobile_template', '', 0);
+
+$mobile = new DetectMobile();
+if ( $mobile->run() == 'mobile' ){
+    if( !empty($use_if) && $use_if ) {
         $modx->setPlaceholder($get_var, 'mobile');
-    } else {
-        $modx->setPlaceholder($get_var, 'full');
+        if ( $mobile_template > 0 ) {
+            $modx->setPlaceholder('mobiletemplate', 'Set template: '.$mobile_template);
+        } else {
+            $modx->setPlaceholder('mobiletemplate', 'No template set');
+        }
     }
+    if ( $mobile_template > 0 ) {
+        $modx->resource->set('template', $mobile_template);
+    }
+    $_SESSION[$get_var] = 'mobile';
+} else {
+    if( !empty($use_if) && $use_if ) {
+        $modx->setPlaceholder($get_var, 'full');
+        if ( $mobile_template > 0 ) {
+            $modx->setPlaceholder('mobiletemplate', 'Default template ');
+        } else {
+            $modx->setPlaceholder('mobiletemplate', 'No template set');
+        }
+    }
+    $_SESSION[$get_var] = 'full';
 }
 return;
